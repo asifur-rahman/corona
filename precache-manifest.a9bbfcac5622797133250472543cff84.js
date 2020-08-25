dbeb60a07647aef990ca5c89dacb91327bbb082b
DemiShinigami <asifurrahman920@gmail.com>
2020-08-25 15:40:22 +0600

    Updates

---
+++ precache-manifest.a9bbfcac5622797133250472543cff84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6663219ebfb4445bc93872d33243d03c",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "bec84ff789ed0a9892e3",
+    "url": "/corona/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "75a024442f28231139db",
+    "url": "/corona/static/css/main.4766afd5.chunk.css"
+  },
+  {
+    "revision": "bec84ff789ed0a9892e3",
+    "url": "/corona/static/js/2.64878431.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/corona/static/js/2.64878431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a024442f28231139db",
+    "url": "/corona/static/js/main.7e0cfa23.chunk.js"
+  },
+  {
+    "revision": "3c498e60ab3e9474f817",
+    "url": "/corona/static/js/runtime-main.f43a888c.js"
+  },
+  {
+    "revision": "860712aa4a647e65d6ac04a1749a7030",
+    "url": "/corona/static/media/background.860712aa.jpg"
+  }
+]);
